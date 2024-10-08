49e6db1a048677dced465ea8969c65389f8d89f2
Filipe Formigari <formigari2804@gmail.com>
2024-10-08 19:50:34 -0300

    a

--- calculadoraTeste/src/test/java/CalculadoraTestParametros.java
+++ calculadoraTeste/src/test/java/CalculadoraTestParametros.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculadoraTestParametros {
--- calculadoraTeste/src/test/java/CalculadoraTestStream.java
+++ calculadoraTeste/src/test/java/CalculadoraTestStream.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-
 import java.util.stream.Stream;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculadoraTestStream {
